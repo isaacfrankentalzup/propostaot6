dc274a3b3dbc9a80bda5773946ed9b2b4248c583
isaacfrankentalzup <isaac.frankental@zup.com.br>
2021-08-09 14:02:39 -0300

    065.como_saber_tudo_funcionando_corretamente.md

---
+++ src/main/java/fase4proposta/cartao/viagens/Viagem.java
@@ -0,0 +1,15 @@
+package fase4proposta.cartao.viagens;
+
+import java.time.LocalDateTime;
+
+public class Viagem {
+    private Long id;
+    private Long idcartao;
+    private String destino;
+    private LocalDateTime inicioViagem;
+    private LocalDateTime terminoViagem;
+    private LocalDateTime dataQueOClienteAvisou;
+    private Long ipCliente;
+    private String userAgent;
+
+}
